8d488debdfbc163d032182f2b8b60ad9ff5caf1e
Vladimir <68743149+vpenywise@users.noreply.github.com>
2021-12-14 14:42:34 +0200

    Update _Stream.java

--- _Stream.java
+++ _Stream.java
@@ -1,19 +1,42 @@
 package streams;
 
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 import java.util.stream.Collectors;
+
+import static streams._Stream.Gender.FEMALE;
+import static streams._Stream.Gender.MALE;
 
 public class _Stream {
 
     public static void main(String[] args) {
 
         List<Person> people = List.of(
-                new Person("John", Gender.MALE),
-                new Person("Maria", Gender.FEMALE),
-                new Person("Aisha", Gender.FEMALE),
-                new Person("Alex", Gender.MALE),
-                new Person("Alice", Gender.FEMALE)
+                new Person("John", MALE),
+                new Person("Maria", FEMALE),
+                new Person("Aisha", FEMALE),
+                new Person("Alex", MALE),
+                new Person("Alice", FEMALE)
         );
+
+        Set<Gender> genderSet = new HashSet();
+        for (Person people1 : people) {
+            Gender gender = people1.gender;
+            genderSet.add(gender);
+        }
+        genderSet.forEach(System.out::println);
+
+        System.out.println();
+
+        HashSet genderSet2 = new HashSet();
+        for (Person people1 : people) {
+            Gender gender = people1.gender;
+            genderSet2.add(gender);
+        }
+        genderSet2.forEach(System.out::println);
+
+        System.out.println();
 
         people.stream()
                 .map(person -> person.gender)
